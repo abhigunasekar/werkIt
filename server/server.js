584e3a8abb3b63f36d4722d9e4b8c3e250c67231
Katy Penquite <cpenquit@purdue.edu>
2021-03-02 13:23:31 -0500

    start create account routing

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 const express = require('express');
-const body_parser = require('body-parser');
 const app = express();
 
-const jsonParser = bodyParser.json();
+app.use(express.json());
+
+var name = "";
+var username = "";
+var password = "";
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.post('/create_account', jsonParser, (req, res) => {
+app.post('/create_account', (req, res) => {
   console.log(req.body); 
-  var account_info = req.body;
+  name = req.body.name;
+  username = req.body.username;
+  password = req.body.password;
   res.send("hello " + account_info.name + "!");
 });
+
+app.patch('/user/:id/profile', (req, res) => {
+    console.log(req.body);
+    if (req.params.id == username) {
+        password = req.body.password;
+        res.send("your new password is " + password);
+    } else {
+        res.send("no user found");
+    }
+})
 
 app.listen(8000, function() {
     console.log("server is running")
