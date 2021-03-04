378b22fa788e3eef07f9e5097d484b1c67a39e95
Abhishek Gunasekar <agunase@xinu02.cs.purdue.edu>
2021-03-04 13:40:37 -0500

    Implemented Confirm Password Functionality -- Abhi

--- web-client/app.js
+++ web-client/app.js
@@ -1,7 +1,7 @@
 function validatePassword() {
     var x = document.forms["signUp"]["pass"].value;
     var y = document.forms["signUp"]["confirmPass"].value;
-    if (x != y)) {
+    if (x != y) {
         alert("Passwords Do Not Match. Try Again!");
         return false;
     }
