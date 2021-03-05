dcf59a4f9e1b8e51f9f75464152bbd2083b069ef
Katy Penquite <cpenquit@purdue.edu>
2021-03-04 20:48:33 -0500

    initial connect server to database

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const app = express();
 const port = 8000;
 
 var mongoose = require("mongoose");
-//mongoose.Promise = global.Promise;mongoose.connect("mongodb+srv://cluster0.aplfp.mongodb.net/WerkItDB"); //alternate address if db connection does not work with default mongo port
-mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/WerkItDB");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://adminUser:adminPassword@cluster0.aplfp.mongodb.net/WerkItDB", {useNewUrlParser: true}); //alternate address if db connection does not work with default mongo port
+//mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/WerkItDB");
 console.log("Connected to WerkIt Database");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,19 +20,27 @@ app.get('/', function (req, res) {
 });
 
 app.post('/create_account', (req, res) => {
-  /*var myData = new User(req.body); 			// Will add this back in
+  console.log(req.body); 
+  name = req.body.f_name + " " + req.body.l_name;
+  username = req.body.username;
+  password = req.body.password;
+  // res.send("Welcome " + name + "!\n\n" + "Please Download the Werk It Mobile App");
+
+  var userSchema = new mongoose.Schema({
+    name: String,
+    user: String,
+    pass: String
+  });
+  var User = mongoose.model("people", userSchema);
+  var myData = new User(req.body); 			// Will add this back in
   myData.save()						// once I can figure out
   .then( item => {					// if server is receiving
 	  res.send("item saved to database");		// input from client
   })
   .catch(err => {
+    console.log(err);
 	  res.status(400).send("unable to save to database");
-  	});*/
-  console.log(req.body); 
-  name = req.body.f_name + " " + req.body.l_name;
-  username = req.body.username;
-  password = req.body.password;
-  res.send("Welcome " + name + "!\n\n" + "Please Download the Werk It Mobile App");
+  	});
 });
 
 app.patch('/user/:id/profile', (req, res) => {
