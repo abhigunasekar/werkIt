0a2fb8974cb332cb149bc26e16953c3ad60f0075
Katy Penquite <cpenquit@purdue.edu>
2021-03-04 23:56:53 -0500

    Save new users to database

--- server/mongoConnect.js
+++ server/mongoConnect.js
@@ -1,22 +1,50 @@
-var express = require('express');
-var app = express();
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost/WerkItDB';
-var str = "";
-console.log("Past mongo db connect");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+// TODO setup new user/pass that is not admin for general users
+mongoose.connect("mongodb+srv://adminUser:adminPassword@cluster0.aplfp.mongodb.net/WerkItDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 
-app.route('/people').get(function(req, res) {
-	MongoClient.connect(url, function(err, db) {
-		var collection = db.collection('name').find();
-		cursor.forEach(function(item) {
-			if (item != null) {
-				str = str + " Name: " + item.name + "</br>";
-			}
-		}, function(err) {
-			res.send(err);
-			db.close();
-			}
-		);
-	});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log("Connected to WerkIt Database");
 });
-var server = app.listen(3000, function() {});
+
+// Schemas
+
+const userSchema = new mongoose.Schema({
+	name: String,
+	user: String,
+	pass: String,
+	email: String,
+});
+
+userSchema.methods.get_pass = function() {
+	console.log(this.pass);
+	return this.pass;
+}
+
+// Functions called by server
+
+const User = mongoose.model('User', userSchema)
+
+function save_new_account_data(u_name, username, password, u_email) {
+	
+	const user = new User({
+		name: u_name,
+		user: username,
+		pass: password,
+		email: u_email
+	});
+
+	user.save(function (err, user) {
+		if (err) return console.error(err);
+		console.log("successfully created new user");
+	});
+}
+
+async function get_user_pass(name) {
+	return await User.find({ name: 'Katy'}).pass;
+}
+
+module.exports = { save_new_account_data, get_user_pass }
+
--- server/server.js
+++ server/server.js
@@ -1,46 +1,27 @@
 const express = require('express');
+const mc = require('./mongoConnect')
 var bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://adminUser:adminPassword@cluster0.aplfp.mongodb.net/WerkItDB", {useNewUrlParser: true}); //alternate address if db connection does not work with default mongo port
-//mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/WerkItDB");
-console.log("Connected to WerkIt Database");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-var name = "";
-var username = "";
-var password = "";
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+// TODO implement return status codes
 app.post('/create_account', (req, res) => {
   console.log(req.body); 
-  name = req.body.f_name + " " + req.body.l_name;
-  username = req.body.username;
-  password = req.body.password;
-  // res.send("Welcome " + name + "!\n\n" + "Please Download the Werk It Mobile App");
+  var name = req.body.f_name + " " + req.body.l_name;
 
-  var userSchema = new mongoose.Schema({
-    name: String,
-    user: String,
-    pass: String
-  });
-  var User = mongoose.model("people", userSchema);
-  var myData = new User(req.body); 			// Will add this back in
-  myData.save()						// once I can figure out
-  .then( item => {					// if server is receiving
-	  res.send("item saved to database");		// input from client
-  })
-  .catch(err => {
-    console.log(err);
-	  res.status(400).send("unable to save to database");
-  	});
+  mc.save_new_account_data(
+    name, req.body.username, req.body.password, req.body.email
+  );
+  res.send("Welcome " + name + "!\n\nPlease Download the Werk It Mobile App");
+  var list = mc.get_user_pass(name);  // TODO pass prints as <pending> -- need to do research on async functions
+  console.log(list);
 });
 
 app.patch('/user/:id/profile', (req, res) => {
