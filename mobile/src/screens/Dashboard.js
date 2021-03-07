0c8c341bc259557e74d0e5cbb198f9b3fe93d62f
Vivek Nair <viveknair360@gmail.com>
2021-03-07 11:40:27 -0500

    Added circular picture view, still need to add file upload option

--- mobile/src/screens/Dashboard.js
+++ mobile/src/screens/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, Text, Image } from 'react-native'
 import Button from '../components/Button';
+import mafioso from '../../assets/MV5BOGQ4NTRhNjMtODYyYi00NjJhLThmZTUtNmI4MTdlZWM5MDliXkEyXkFqcGdeQXVyMzMzMTExNzI@._V1_UY1200_CR109,0,630,1200_AL_.jpg';
 
 export default class Dashboard extends Component{
     constructor() {
@@ -14,9 +15,14 @@ export default class Dashboard extends Component{
     render() {
         return(
             <ScrollView>
+                <Image
+                    source={mafioso}
+                    style={{width: 200, height: 200, borderRadius: 200/ 2}}
+                    />
+                {/* <img src={mafioso} alt="Slow but sure" /> */}
                 <Text>Dashboard</Text>
                 <Button
-                    buttonText='Create New Workout'
+                    buttonText="Create New Workout"
                     onPress={() => this.props.navigation.navigate('WorkoutEditor')}
                 />
             </ScrollView>
