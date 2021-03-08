0bfd9100a3a3c7f6ac24d10eaa575de860406524
Vivek Nair <viveknair360@gmail.com>
2021-03-08 11:25:35 -0500

    Added Error Message Functionality in User Story 5 - Mobile App. This is done in the same manner as in User Story 4 when the credentials already exist.

--- mobile/src/screens/Login.js
+++ mobile/src/screens/Login.js
@@ -13,24 +13,47 @@ export default class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            errUsername: 'Username',
+            errUsernameStyle: 'textBox',
+            errPassword: 'Password',
+            errPasswordStyle: 'textBox',
         };
+
+        this.opError = this.opError.bind(this);
     }
+
+    opError(errorString) {
+
+        // Add Code for when the email, username, or password already exist for a user in the database
+
+        if (errorString === "Test") {
+            this.setState({errUsername : 'Error: Username is wrong! Please try again:'});
+            this.setState({errUsernameStyle : 'textBoxErr'});
+        }
+        
+        if (errorString === "Test") {
+            this.setState({errPassword : 'Error: Password is wrong! Please try again:'});
+            this.setState({errPasswordStyle : 'textBoxErr'});
+        }
+    };
 
     render() {
         return (
             <View style={styles.loginContainer}>
                 <View style={styles.loginForm}>
                     <TextBox
-                        placeholder='Username'
+                        placeholder={this.state.errUsername}
                         onChangeText={(text) => this.setState({ username: text })}
                         value={this.state.username}
-                        style={styles.textBox}
+                        // style={styles.textBox}
+                        style={styles[this.state.errUsernameStyle]}
                     />
                     <TextBox
-                        placeholder='Password'
+                        placeholder={this.state.errPassword}
                         onChangeText={(text) => this.setState({ password: text })}
                         value={this.state.password}
-                        style={styles.textBox}
+                        // style={styles.textBox}
+                        style={styles[this.state.errPasswordStyle]}
                     />
                     <Text onPress={() => this.props.navigation.navigate('CreateAccount')} style={{ marginBottom: 10, color: '#7641BD'}}>Create Account</Text>
                     <Button 
@@ -40,6 +63,10 @@ export default class Login extends Component {
                             console.log('Password: ' + this.state.password);
                             this.props.login();
                         }}
+                    />
+                    <Button
+                        buttonText='Dev: Test Error Messages'
+                        onPress={() => this.opError("Test")}
                     />
                 </View>
             </View>
