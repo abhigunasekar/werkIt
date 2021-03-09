905989d670f1c5a1579a082940a392202b9c042f
Katy Penquite <cpenquit@purdue.edu>
2021-03-09 17:44:56 -0500

    connect mobile client to server

--- mobile/src/ServerMethods.js
+++ mobile/src/ServerMethods.js
@@ -15,13 +15,24 @@ export async function createAccount(info) {
 
     console.log('attempt server connection');
 
-    fetch('http://127.0.0.1:8000/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    })
-        .then(response => console.log('connect'))
-        .catch(error => console.log(error));
+    // fetch('http://127.0.0.1:8000/', {
+    //     method: 'GET',
+    //     mode: 'cors',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Accept': 'application/json'
+    //     },
+    //     params: null,
+    //     body: null
+    // })
+     fetch('http://10.186.118.49:8000/')
+         .then(response => console.log('connect'))
+         .catch(error => console.log(error));
+    console.log("made it");
+
+    // const response = await fetch("http://192.168.137.1:8000/");
+    // console.log("here");
+    // const data = await response.json();
+    // console.log("now here");
+    // console.log(data);
 }
