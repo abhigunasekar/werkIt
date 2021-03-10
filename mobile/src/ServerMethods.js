b23ceeffedcc0370494f6030d0cb08973b9c1265
Christopher Yu <chris.b.yu@gmail.com>
2021-03-09 19:36:27 -0500

    server stuff

--- mobile/src/ServerMethods.js
+++ mobile/src/ServerMethods.js
@@ -15,24 +15,8 @@ export async function createAccount(info) {
 
     console.log('attempt server connection');
 
-    // fetch('http://127.0.0.1:8000/', {
-    //     method: 'GET',
-    //     mode: 'cors',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Accept': 'application/json'
-    //     },
-    //     params: null,
-    //     body: null
-    // })
-     fetch('http://10.186.118.49:8000/')
+    fetch('http://10.186.150.93:8000/')
          .then(response => console.log('connect'))
          .catch(error => console.log(error));
     console.log("made it");
-
-    // const response = await fetch("http://192.168.137.1:8000/");
-    // console.log("here");
-    // const data = await response.json();
-    // console.log("now here");
-    // console.log(data);
 }
