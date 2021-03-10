ac7ed58b3d53af2e235e9915df3ee1e7edfd55e7
Christopher Yu <chris.b.yu@gmail.com>
2021-03-10 17:10:23 -0500

    working on editing workouts

---
+++ mobile/src/components/WorkoutLabel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Text, Pressable } from 'react-native';
+
+export default class WorkoutLabel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: this.props.name,
+            exercises: [],
+        }
+    }
+
+    render() {
+        return(
+            <Pressable style={{borderColor: '#000000', /*borderWidth: 3,*/ width: '100%', marginBottom: 7}} onPress={() => this.props.edit()}>
+                <Text>{this.props.name}</Text>
+            </Pressable>
+        );
+    }
+}
