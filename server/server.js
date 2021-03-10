86e3d719b71650c03c13c0d39e288352fe7cb45a
Katy Penquite <cpenquit@purdue.edu>
2021-03-09 20:05:44 -0500

    Validate username for create account/update routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const util = require('util');
-const mc = require('./mongoConnect');
-var bodyParser = require('body-parser');
+const mc = require('./mongoConnect')
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
-const ip = "10.186.118.49";
+const ip = "10.0.0.48";
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,17 +14,21 @@ app.get('/', function (req, res) {
   res.send('Successful connection to werkIt server')
 });
 
-// TODO implement return status codes
 app.post('/create_account', (req, res) => {
   console.log(req.body); 
   var name = req.body.f_name + " " + req.body.l_name;
-
-  mc.save_new_account_data(
-    name, req.body.username, req.body.password, req.body.email
-  );
-  res.send("Welcome " + name + "!\n\nPlease Download the Werk It Mobile App");
-  var list = mc.get_user_pass(name);  // TODO pass prints as <pending> -- need to do research on async functions
-  console.log(list);
+  mc.check_user_existence(req.body.username).then(exists => {
+    if (exists) {
+      console.log("Username already exists.")
+      res.status(403).send("Username already exists in database.  Please choose a new one.");
+    } else {
+      mc.save_new_account_data(
+        name, req.body.username, req.body.password, req.body.email
+      );
+      console.log("Successfully created new user.")
+      res.status(201).send("Welcome " + name + "!\n\nPlease Download the Werk It Mobile App");
+    }
+  });
 });
 
 app.patch('/user/:id/profile', (req, res) => {
@@ -38,5 +42,5 @@ app.patch('/user/:id/profile', (req, res) => {
 })
 
 app.listen(port, ip, function() {
-    console.log(util.format("Server listening on %s:%d", ip, port));
+    console.log(util.format("Server listening on http://%s:%d", ip, port));
 })
