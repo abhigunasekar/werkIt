39bb6819e7d3c7c48b21525797f4e3748d776d68
Mr.CEO <agunase@purdue.du>
2021-03-31 22:12:23 -0400

    Done with User Story #20

--- server/server.js
+++ server/server.js
@@ -13,8 +13,9 @@ const port = 8000;
 // TODO set ip dynamically or figure out how to run server
 // from anywhere - must match network used by expo though
 
-const ip = "127.0.0.1";
+const ip = "128.10.25.205";
 var urlencodedparser = bodyParser.urlencoded({ extended: false })
+app.use(cors())
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -28,6 +29,7 @@ app.use(cookieParser());
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
 app.use(methodOverride('_method'));
+
 
 // check connection with server
 app.get('/', function(req, res) {
@@ -114,97 +116,97 @@ app.patch('/profile/:username/:field', (req, res) => {
 })
 
 // update dark mode
-app.patch('/user/:username/darkmode', (req, res) => {
-  console.log("here");
-  console.log("Updating dark mode for user: " + req.params.username);
-  mc.update_darkmode(req.params.username).then(user => {
-    console.log("Successfully updated dark mode value to: " + user.dark_mode);
-    res.status(200).end();
-  });
+app.patch('/user/:username/darkmode', urlencodedparser, cors(), (req, res) => {
+    console.log("here");
+    console.log("Updating dark mode for user: " + req.params.username);
+    mc.update_darkmode(req.params.username).then(user => {
+        console.log("Successfully updated dark mode value to: " + user.dark_mode);
+        res.status(200).end();
+    });
 });
 
 // get known workout types
 app.get('/:username/workoutTypes', (req, res) => {
-  console.log("Requesting known workout types");
-  mc.get_workout_types(req.params.username).then(types => {
-    console.log("Found known workout types: " + types);
-    res.status(200).json(types);
-  });
+    console.log("Requesting known workout types");
+    mc.get_workout_types(req.params.username).then(types => {
+        console.log("Found known workout types: " + types);
+        res.status(200).json(types);
+    });
 });
 
 // set new workout type
 // bug fixed??
 app.post('/:username/workoutType', (req, res) => {
-  console.log("Saving new workout type to database for user: %s", req.params.username);
-  mc.save_new_workoutType(
-    req.params.username, req.body.name, req.body.exercises
-  ).then(_ => {
-    console.log("Successfully saved new workoutType");
-    res.status(200).end();
-  })
+    console.log("Saving new workout type to database for user: %s", req.params.username);
+    mc.save_new_workoutType(
+        req.params.username, req.body.name, req.body.exercises
+    ).then(_ => {
+        console.log("Successfully saved new workoutType");
+        res.status(200).end();
+    })
 })
 
 // get exercises given a workout type
 app.get('/:username/:workoutType/exercises', (req, res) => {
-  console.log("Getting exercises for workoutType: %s", req.params.workoutType);
-  mc.get_exercises_for_type(req.params.username, req.params.workoutType).then(ex_list => {
-    console.log("Exercises found");
-    res.status(200).json(ex_list);
-  });
+    console.log("Getting exercises for workoutType: %s", req.params.workoutType);
+    mc.get_exercises_for_type(req.params.username, req.params.workoutType).then(ex_list => {
+        console.log("Exercises found");
+        res.status(200).json(ex_list);
+    });
 })
 
 // add new exercise to a given workout type
 app.put('/:username/:workoutType/exercise', (req, res) => {
-  console.log("Adding exercise to workoutType: %s", req.params.workoutType);
-  mc.save_new_exerciseType(
-    req.params.username, req.params.workoutType, req.body.name, req.body.data
-  ).then(_ => {
-    console.log("Successfully added exercise to workoutType");
-    res.status(200).end();
-  });
+    console.log("Adding exercise to workoutType: %s", req.params.workoutType);
+    mc.save_new_exerciseType(
+        req.params.username, req.params.workoutType, req.body.name, req.body.data
+    ).then(_ => {
+        console.log("Successfully added exercise to workoutType");
+        res.status(200).end();
+    });
 });
 
 // save a new workout
 app.post('/:username/workout', (req, res) => {
-  console.log("saving new workout");
-  console.log(req.body);
-  console.log(req.params);
-  mc.save_workout(req.params.username, req.body.name, req.body.type, req.body.exercises).then(_ => {
-    res.status(200).end()
-  });
+    console.log("saving new workout");
+    console.log(req.body);
+    console.log(req.params);
+    mc.save_workout(req.params.username, req.body.name, req.body.type, req.body.exercises).then(_ => {
+        res.status(200).end()
+    });
 });
 
 
 // get workouts for a user
 app.get('/:username/workouts', (req, res) => {
-  console.log("Requesting workouts");
-  mc.get_workouts(req.params.username).then(wkouts => {
-    console.log("Found workouts: " + wkouts);
-    res.status(200).json(wkouts);
-  });
+    console.log("Requesting workouts");
+    mc.get_workouts(req.params.username).then(wkouts => {
+        console.log("Found workouts: " + wkouts);
+        res.status(200).json(wkouts);
+    });
 })
 
 // get one workout/exercises by name
 app.get('/:username/:workout', (req, res) => {
-  console.log("Getting workout and repective exercises");
-  mc.get_workout_data(req.params.username, req.params.workout).then(wkout => {
-    console.log("Found workout:" + wkout);
-    res.status(200).json(wkout);
-  })
+    console.log("Getting workout and repective exercises");
+    mc.get_workout_data(req.params.username, req.params.workout).then(wkout => {
+        console.log("Found workout:" + wkout);
+        res.status(200).json(wkout);
+    })
 })
 
 // TODO update workout
 app.patch('/:username/:workout', (req, res) => {
-  
+
 })
 
 // create weekly plan
 app.post('/:username/plan', (req, res) => {
-  console.log("Saving weekly workout plan");
-  mc.save_workout_plan(req.params.username, req.body).then(_ => {
-    console.log("Successfully saved workout plan");
-    res.status(200).end();
-  })
+    console.log("Saving weekly workout plan");
+    mc.save_workout_plan(req.params.username, req.body).then(_ => {
+        console.log("Successfully saved workout plan");
+        res.status(200).end();
+    })
 })
 
 app.listen(port, ip, function() {
