f61b6673debaa154188ed2db4796c05efd61d096
Katy Penquite <cpenquit@purdue.edu>
2021-04-01 23:25:44 -0400

    change ip addresses

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = 8000;
 // TODO set ip dynamically or figure out how to run server
 // from anywhere - must match network used by expo though
 
-const ip = "128.10.25.205";
+const ip = "10.184.11.114";
 var urlencodedparser = bodyParser.urlencoded({ extended: false })
 app.use(cors())
 
