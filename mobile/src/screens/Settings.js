3e3a6f5399100ac72f2e360fc9da89e733f09645
Christopher Yu <chris.b.yu@gmail.com>
2021-04-09 11:31:58 -0400

    stash

--- mobile/src/screens/Settings.js
+++ mobile/src/screens/Settings.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Switch } from 'react-native';
 
 import Button from '../components/Button';
 
 export default class Settings extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            darkMode = false,
+        }
     }
 
     render() {
         return(
             <View style={{alignItems: 'center', paddingTop: 300}}>
                 <Text>NOT YET IMPLEMENTED</Text>
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={this.state.darkMode ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={() => this.setState({ darkMode: !this.state.darkMode })}
+                    value={this.state.darkMode}
+                />
                 <Button
                     buttonText='Log out'
                     onPress={() => this.props.logout()}
