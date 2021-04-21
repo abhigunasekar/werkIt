c58bee832b0514bc3e5c7788d8a36dba3afe238a
Dithi Saxena <saxena52@purdue.edu>
2021-04-20 20:36:11 -0500

    Fixed stupid mistake

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = 8000;
 // TODO set ip dynamically or figure out how to run server
 // from anywhere - must match network used by expo though
 
-const ip = "127.0.0.1";
+const ip = "10.0.0.86";
 var urlencodedparser = bodyParser.urlencoded({ extended: false })
 app.use(cors())
 
