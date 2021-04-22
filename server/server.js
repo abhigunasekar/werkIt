87ad67124ce493d781ba2030b9f0a02adcf84d3c
Abhishek Gunasekar <abhishekgunasekar@Abhisheks-MacBook-Pro.local>
2021-04-21 21:50:27 -0400

    Fixed Server port

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = 8000;
 // TODO set ip dynamically or figure out how to run server
 // from anywhere - must match network used by expo though
 
-const ip = "10.0.0.48";
+const ip = "127.0.0.1";
 var urlencodedparser = bodyParser.urlencoded({ extended: false })
 app.use(cors())
 
